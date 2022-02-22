42a65279dce118a8c9bcc30fa58e9e637e7db706
Kyle Regan <33530244+ktrregan@users.noreply.github.com>
2022-02-22 12:37:04 -0600

    Add JavaScript to handle wiring up custom links inserted into Drupal

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/**
+ * @file script.js
+ */
+(function (Drupal, $) {
+  'use strict';
+
+  Drupal.behaviors.adcAccessibe = {
+    attach: function(context, settings) {
+      var $acsbLinks = $('a[href="#showacsb"]');
+      $acsbLinks.each(function(i, link) {
+        $(link).attr('data-acsb-custom-trigger="true"', 'true');
+        $(link).attr('href', 'javascript:void(0);');
+      });
+    }
+  };
+
+}(Drupal, jQuery));
